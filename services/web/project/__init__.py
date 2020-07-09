dfcac4adfe9e7104f9070a9bf668ed32a22231ed
alec <alec@implicitdesign.co.za>
2020-07-09 15:26:13 +0200

    BeepPete has joined the chat

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,9 +1,26 @@
-from flask import Flask, jsonify
+import os
+import json
+from slackeventsapi import SlackEventAdapter
+
+from threading import Thread
+
+from slack import WebClient
+
+from flask import Flask, jsonify, Response
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config.from_object("project.config.Config")
 db = SQLAlchemy(app)
+
+SLACK_SIGNING_SECRET = os.getenv("SLACK_SIGNING_SECRET")
+SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
+
+
+client = WebClient(token=SLACK_BOT_TOKEN)
+
+greetings = ["hi", "hello", "hello there", "hey"]
 
 
 class User(db.Model):
@@ -17,6 +34,42 @@ class User(db.Model):
         self.email = email
 
 
+# @app.route("/")
+# def index():
+#     return jsonify(hello="world")
+
+
 @app.route("/")
-def hello_world():
-    return jsonify(hello="world")
+def event_hook(request):
+    json_dict = json.loads(request.body.decode("utf-8"))
+    # if json_dict["token"] != VERIFICATION_TOKEN:
+    #     return {"status": 403}
+    if "type" in json_dict:
+        if json_dict["type"] == "url_verification":
+            response_dict = {"challenge": json_dict["challenge"]}
+            return response_dict
+    return {"status": 500}
+
+
+slack_events_adapter = SlackEventAdapter(
+    SLACK_SIGNING_SECRET, "/slack", app
+)
+
+
+@slack_events_adapter.on("app_mention")
+def handle_message(event_data):
+    def send_reply(value):
+        event_data = value
+        message = event_data["event"]
+        if message.get("subtype") is None:
+            command = message.get("text")
+            channel_id = message["channel"]
+            if any(item in command.lower() for item in greetings):
+                message = (
+                    "Hello <@%s>! :tada:"
+                    % message["user"]  # noqa
+                )
+                client.chat_postMessage(channel=channel_id, text=message)
+    thread = Thread(target=send_reply, kwargs={"value": event_data})
+    thread.start()
+    return Response(status=200)
